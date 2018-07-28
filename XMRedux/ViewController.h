33fbcac417c94e6e5892183474dc7bbe9b381b5b
Mifit <mifit@mifitdeimac.local>
2018-07-28 08:53:47 +0800

    Initial Commit

---
+++ XMRedux/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMRedux
+//
+//  Created by Mifit on 2018/7/28.
+//  Copyright © 2018年 Mifit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMReduxTests/XMReduxTests.m
@@ -0,0 +1,39 @@
+//
+//  XMReduxTests.m
+//  XMReduxTests
+//
+//  Created by Mifit on 2018/7/28.
+//  Copyright © 2018年 Mifit. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XMReduxTests : XCTestCase
+
+@end
+
+@implementation XMReduxTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
